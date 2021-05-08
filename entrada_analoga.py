7b3e07d52ef7b2f253e734cd5013ad719ecf53b6
ingDavidRivera <davidriveraarbelaez@gmail.com>
2021-05-07 21:01:19 -0500

    Add files via upload

--- entrada_analoga.py
+++ entrada_analoga.py
@@ -1,7 +1,7 @@
 import machine # Importa la librería machine
 import time # Importa la librería tiempo
 
-potenciometro = machine.ADC(26) # GPIO 26 es el primer canal analógico (ADC0) en el conversor análogo - digital
+potenciometro = machine.ADC(26) # GPIO 26 como primer canal ADC0 en el conversor análogo - digital
 
 while True:
     print(potenciometro.read_u16()) # Muestra el valor leído por el potenciómetro
